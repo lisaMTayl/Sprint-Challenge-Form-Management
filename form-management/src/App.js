3de490aa82fc5d1ae7db2abbbf94c0e6f61c4ac1
Lisa <lmkirk@gmail.com>
2019-08-09 13:43:13 -0400

    added one test

--- form-management/src/App.js
+++ form-management/src/App.js
@@ -4,7 +4,7 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
+    <div data-testid='renderedForm' className="App">
       <FormikRegForm />
     </div>
   );
--- form-management/src/components/RegForm.test.js
+++ form-management/src/components/RegForm.test.js
@@ -1,10 +1,11 @@
 import React from 'react';
-import { render, fireEvent } from 'react-testing-library';
-import 'react-testing-library/cleanup-after-each';
+import { render, fireEvent } from '@testing-library/react';
+import "@testing-library/react/cleanup-after-each"
 import RegForm from './RegForm';
 
-describe('<RegForm />', () => {
+describe('<FormikRegForm />', () => {
   it('renders without crashing', () => {
     render(<RegForm/>)
   })
+
 });
